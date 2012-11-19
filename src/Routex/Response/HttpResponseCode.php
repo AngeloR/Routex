2093407499788c09be20cde8a50067d8f2340a38
AngeloR <xangelo@gmail.com>
2012-11-19 21:52:30 +0000

    updated with constants for codes

--- src/Routex/Response/HttpResponseCode.php
+++ src/Routex/Response/HttpResponseCode.php
@@ -8,14 +8,14 @@ namespace Routex\Response;
  */
 class HttpResponseCode {
 	
-	public $OK = 200;
-	public $CREATED = 201;
-	public $MOVED_PERMANENTLY = 301;
-	public $FOUND = 302;
-	public $BAD_REQUEST = 400;
-	public $UNAUTHORIZED = 401;
-	public $FORBIDDEN = 403;
-	public $NOT_FOUND = 404;
-	public $ENTITY_TOO_LARGE = 413;
-	public $NOT_IMPLEMENTED = 501;
+	constant $OK = 200;
+	constant $CREATED = 201;
+	constant $MOVED_PERMANENTLY = 301;
+	constant $FOUND = 302;
+	constant $BAD_REQUEST = 400;
+	constant $UNAUTHORIZED = 401;
+	constant $FORBIDDEN = 403;
+	constant $NOT_FOUND = 404;
+	constant $ENTITY_TOO_LARGE = 413;
+	constant $NOT_IMPLEMENTED = 501;
 }
