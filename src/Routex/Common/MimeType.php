c814087cfecafe88a213a7546e562f90182a8a8a
AngeloR <xangelo@gmail.com>
2012-11-21 17:16:12 +0000

    made mimetypes constants so that they cant be modified

--- src/Routex/Common/MimeType.php
+++ src/Routex/Common/MimeType.php
@@ -10,28 +10,28 @@ namespace Routex\Common;
 class MimeType {
 
 	// Plain-text formats
-	public static $css = 'text/css';
-	public static $html = 'text/html';
-	public static $text = 'text/plain';
-	public static $xml = 'text/xml';
+	constant $CSS = 'text/css';
+	constant $HTML = 'text/html';
+	constant $TEXT = 'text/plain';
+	constant $XML = 'text/xml';
 
 	// Images
-	public static $gif = 'image/gif';
-	public static $jpg = 'image/jpg';
-	public static $png = 'image/png';
-	
+	constant $GIF = 'image/gif';
+	constant $JPG = 'image/jpg';
+	constant $PNG = 'image/png';
+
 	// Video
-	public static $flash = 'video/x-flv';
-	public static $ogg = 'video/ogg';
-	public static $webm = 'video/webm';
+	constant $FLASH = 'video/x-flv';
+	constant $OGG = 'video/ogg';
+	constant $WEBM = 'video/webm';
 
 	// Application
-	public static $application = 'application/octet-stream';
-	public static $js = 'application/javascript';
-	public static $json = 'application/json';
-	public static $pdf = 'application/pdf';
-	public static $res = 'application/rss+xml';
-	public static $xhtml = 'application/xhtml+xml';
-	public static $zip = 'application/zip';
+	constant $APPLICATION = 'application/octet-stream';
+	constant $JS = 'application/javascript';
+	constant $JSON = 'application/json';
+	constant $PDF = 'application/pdf';
+	constant $RES = 'application/rss+xml';
+	constant $XHTML = 'application/xhtml+xml';
+	constant $ZIP = 'application/zip';
 	
 }
